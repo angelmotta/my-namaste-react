aac40c414b8708b5c668e467e3b48842c3b988dc
Angel Motta <angel.motta@utec.edu.pe>
2023-11-07 19:05:34 -0500

    refactor code structure

--- E04-TalkIsCheap/food-order-app/src/Components/Pedidos.js
+++
@@ -1,9 +0,0 @@
-const Pedidos = () => {
-    return (
-        <div>
-            <h1>Haz tu pedido</h1>
-        </div>
-    );
-};
-
-export default Pedidos;
--- E04-TalkIsCheap/food-order-app/src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const CDN_URL = "https://images.rappi.pe/products/";
-export const LOGO_URL = "TO_BE_DEFINED";
