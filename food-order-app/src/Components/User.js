6bcab87e6179a9c4aca0cbe74a58b8beaae4dbce
Angel Motta <angel.motta@utec.edu.pe>
2023-11-29 22:42:47 -0500

    feat: props in class based components

--- food-order-app/src/Components/User.js
+++ food-order-app/src/Components/User.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+    const [followers] = useState(10);
+    const [following] = useState(0);
+
     return (
         <div className="user-card">
             <h4>Name: {name}</h4>
             <h4>Location: Lima, Perú</h4>
             <h4>Contact: @angelmottap</h4>
+            <h4>Follwers: {followers}</h4>
+            <h4>Follwing: {following}</h4>
         </div>
     );
 };
--- food-order-app/src/Components/UserClass.js
+++ food-order-app/src/Components/UserClass.js
@@ -3,15 +3,22 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            followers: 100,
+            following: 200,
+        };
     }
 
     render() {
         const { name, location } = this.props;
+        const { followers, following } = this.state;
         return (
             <div className="user-card">
                 <h4>Name: {name}</h4>
                 <h4>Location: {location}</h4>
                 <h4>Contact: @angelmottap</h4>
+                <h4>Followers: {followers}</h4>
+                <h4>Following: {following}</h4>
             </div>
         );
     }
